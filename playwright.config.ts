b0752c283685c2991bb97eff837c37aec0dbbbee
Francisco Alves <Franciscoalvescabral22@gmail.com>
2025-10-25 14:07:52 -0300

    chore: renomear arquivo de teste para login.spec.ts e ajustar config do Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,13 +43,13 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
+    /*
+        {
+          name: 'webkit',
+          use: { ...devices['Desktop Safari'] },
+        },
+    
+        /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
